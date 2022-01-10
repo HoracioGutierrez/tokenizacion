fdcf26ede3558acc06105eba28c1c24658cbf782
Horacio Gutierrez <horacio.estevez@gmail.com>
2022-01-10 19:08:45 -0300

    Start with table swatches but remains incomplete...

--- src/pages/Swatches.js
+++ src/pages/Swatches.js
@@ -4,24 +4,89 @@ import { Link } from "react-router-dom"
 import { Controller, useForm } from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 const Swatches = () => {
+
+    const rows = [
+        {
+            id: 1,
+            token : "ACF",
+            address : "0xACF",
+            cantidad : "10",
+            precio : "10",
+        },
+        {
+            id: 2,
+            token : "BCF",
+            address : "0xACF",
+            cantidad : "10",
+            precio : "20",
+        },
+        {
+            id: 3,
+            token : "DCF",
+            address : "0xACF",
+            cantidad : "10",
+            precio : "30",
+        },
+        {
+            id: 4,
+            token : "USDT",
+            address : "0xACF",
+            cantidad : "10",
+            precio : "40",
+        }
+    ]
+
+    const columns = [
+        {
+            field : "id",
+            headerName : "ID",
+            disableColumnMenu : true,
+            width : 50
+        },
+        {
+            field : "token",
+            headerName : "Token",
+            disableColumnMenu : true,
+            flex : 1
+        },
+        {
+            field : "address",
+            headerName : "Address",
+            disableColumnMenu : true,
+            flex : 1
+        },
+        {
+            field : "cantidad",
+            headerName : "Cantidad",
+            disableColumnMenu : true,
+            flex : 1
+        },
+        {
+            field : "precio",
+            headerName : "Precio",
+            flex : 1
+        }
+
+    ]
 
 
     const schema = yup.object().shape({
         nombre: yup.string().required("El nombre es obligatorio").max(50, "El nombre no puede tener mas de 50 caracteres").min(3, "El nombre debe tener al menos 3 caracteres").matches(/^[a-zA-Z\s]*$/, "El nombre solo puede contener letras"),
         dni: yup.string().required("El DNI es obligatorio").max(10, "El DNI no puede tener mas de 9 caracteres").min(8, "El DNI debe tener al menos 8 caracteres").matches(/^[0-9]{1,2}-|.?[0-9]{3}-|.?[0-9]{3}$/, "El DNI no tiene el formato correcto"),
-        pais : yup.string().required("El pais es obligatorio").max(50, "El pais no puede tener mas de 50 caracteres").min(3, "El pais debe tener al menos 3 caracteres").matches(/^[a-zA-Z\s]*$/, "El pais solo puede contener letras"),
-        provincia : yup.string().required("La provincia es obligatoria").max(50, "La provincia no puede tener mas de 50 caracteres").min(3, "La provincia debe tener al menos 3 caracteres").matches(/^[a-zA-Z\s]*$/, "La provincia solo puede contener letras"),
+        pais: yup.string().required("El pais es obligatorio").max(50, "El pais no puede tener mas de 50 caracteres").min(3, "El pais debe tener al menos 3 caracteres").matches(/^[a-zA-Z\s]*$/, "El pais solo puede contener letras"),
+        provincia: yup.string().required("La provincia es obligatoria").max(50, "La provincia no puede tener mas de 50 caracteres").min(3, "La provincia debe tener al menos 3 caracteres").matches(/^[a-zA-Z\s]*$/, "La provincia solo puede contener letras"),
     })
 
     const { register, control, handleSubmit, formState } = useForm({
         defaultValues: {
             nombre: "",
             dni: "",
-            pais : "",
-            provincia : "",
+            pais: "",
+            provincia: "",
         },
         resolver: yupResolver(schema)
     })
@@ -73,24 +138,36 @@ const Swatches = () => {
             <div className="spacing" />
             <Box component="form" onSubmit={handleSubmit(onSubmit)}>
                 <Controller name="nombre" control={control} render={({ field }) => (
-                    <TextField {...field} error={errors.nombre?true:false} placeholder="Nombre" helperText={errors.nombre?.message} fullWidth className="input-control" />
+                    <TextField {...field} error={errors.nombre ? true : false} placeholder="Nombre" helperText={errors.nombre?.message} fullWidth className="input-control" />
                 )} />
                 <Controller name="dni" control={control} render={({ field }) => (
-                    <TextField {...field} error={errors.dni?true:false} placeholder="DNI" helperText={errors.dni?.message} fullWidth className="input-control" />
+                    <TextField {...field} error={errors.dni ? true : false} placeholder="DNI" helperText={errors.dni?.message} fullWidth className="input-control" />
                 )} />
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6}>
                         <Controller name="pais" control={control} render={({ field }) => (
-                            <TextField {...field} error={errors.pais?true:false} placeholder="Pais" helperText={errors.pais?.message} fullWidth className="input-control" />
+                            <TextField {...field} error={errors.pais ? true : false} placeholder="Pais" helperText={errors.pais?.message} fullWidth className="input-control" />
                         )} />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Controller name="provincia"  control={control} render={({ field }) => (
-                            <TextField {...field} error={errors.provincia?true:false} placeholder="Provincia" helperText={errors.provincia?.message} fullWidth className="input-control" />
+                        <Controller name="provincia" control={control} render={({ field }) => (
+                            <TextField {...field} error={errors.provincia ? true : false} placeholder="Provincia" helperText={errors.provincia?.message} fullWidth className="input-control" />
                         )} />
                     </Grid>
                 </Grid>
                 <Button variant='contained' type="submit">Submit</Button>
+            </Box>
+            <div className="spacing" />
+            <div className="spacing" />
+            <Box height={300}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={3}
+                    rowsPerPageOptions={[3]}
+                    checkboxSelection
+                    disableSelectionOnClick
+                />
             </Box>
         </div>
     )
